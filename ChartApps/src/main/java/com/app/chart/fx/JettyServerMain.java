804602b8a423bebca71da1204647c30b24482cce
sandeepreddy1945 <b.sandeepreddy209@gmail.com>
2018-04-24 22:55:18 +0530

    Second Commit
    Signed-off-by: sandeepreddy1945 <b.sandeepreddy209@gmail.com>

--- ChartApps/src/main/java/com/app/chart/fx/JettyServerMain.java
+++ ChartApps/src/main/java/com/app/chart/fx/JettyServerMain.java
@@ -1,31 +1,20 @@
 package com.app.chart.fx;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
-
-import java.io.IOException;
-
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 import com.rest.test.CORSFilter;
 import com.rest.test.EntryPoint;
 
-public class HelloWorld extends AbstractHandler {
-	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-		response.setContentType("text/html;charset=utf-8");
-		response.setStatus(HttpServletResponse.SC_OK);
-		baseRequest.setHandled(true);
-		response.getWriter().println("<h1>Hello World</h1>");
-	}
+public class JettyServerMain {
 
 	public static void main(String[] args) throws Exception {
 
+		startServer();
+	}
+
+	private static void startServer() throws Exception, InterruptedException {
 		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 		context.setContextPath("/");
 		Server server = new Server(8020);
