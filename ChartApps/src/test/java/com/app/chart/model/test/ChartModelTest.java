bff86800f4bd43225b138c11af24ec376350cf02
sandeepreddy1945 <b.sandeepreddy209@gmail.com>
2018-04-25 00:19:26 +0530

    Signed-off-by: sandeepreddy1945 <b.sandeepreddy209@gmail.com>

---
+++ ChartApps/src/test/java/com/app/chart/model/test/ChartModelTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.app.chart.model.test;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+/**
+ * @author Sandeep
+ *
+ */
+public class ChartModelTest {
+
+	Random random = new Random();
+	
+	
+	@Test
+	public void testPojos() {
+		
+	}
+}
