5ae893dca2d9cbde87f71cec441b01d49fcbc200
sandeepreddy1945 <b.sandeepreddy209@gmail.com>
2018-05-28 19:31:22 +0530

    Signed-off-by: sandeepreddy1945 <b.sandeepreddy209@gmail.com>

---
+++ ChartApps/src/main/java/com/app/chart/customer/ui/CustomerUI.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.app.chart.customer.ui;
+
+import javafx.scene.layout.HBox;
+
+/**
+ * @author Sandeep Reddy Battula
+ *
+ */
+public class CustomerUI extends HBox{
+
+	/**
+	 * 
+	 */
+	public CustomerUI() {
+		super(5);
+	}
+
+	
+}
